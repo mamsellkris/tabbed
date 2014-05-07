de220711a9556a7fe2ed51c522ad2a173dd482d4
Kristin ann Regndotter <mistresskris@i2pmail.org>
2014-05-07 11:00:31 -0700

    add ShiftMask to kill client key

--- config.def.h
+++ config.def.h
@@ -51,7 +51,7 @@ static Key keys[] = { \
 	{ MODKEY,                       XK_9,      move,           { .i = 8 } },
 	{ MODKEY,                       XK_0,      move,           { .i = 9 } },
 
-	{ MODKEY,                       XK_q,      killclient,     { 0 } },
+	{ MODKEY|ShiftMask,             XK_q,      killclient,     { 0 } },
 
 	{ 0,                            XK_F11,    fullscreen,     { 0 } },
 };
